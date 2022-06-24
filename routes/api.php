430891915c2c91ed44836b9e1ba18b2c314d9122
Yasser Ninal <yaserninal.120@gmail.com>
2022-06-24 10:50:06 +0300

    h

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user()->name;
 });
 //user route
 Route::post('user/login','App\Http\Controllers\Controller@login');
