4fc7fc83cbd1dc355d2fba2f11ee00220ca77c14
Yasser Ninal <yaserninal.120@gmail.com>
2022-06-29 23:48:19 +0300

    asl

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,8 +37,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            // 'root' => public_path(),
+            // 'root' => storage_path('app/public'),
+            'root' => public_path(),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
