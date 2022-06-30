1f664cee6f27f620e0b8332ff777634ba94b720a
Yasser Ninal <yaserninal.120@gmail.com>
2022-06-30 11:42:14 +0300

    asl

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -97,7 +97,7 @@ class Controller extends BaseController
                 $file = $request->file('image');
                 $extention = $file->getClientOriginalExtension();
                 $filename = time().'.'.$extention;
-                $file->move('public/images/',$filename);
+                $file->move('public/images/'.$filename);
                 $creaetUser->image = $filename;
                 echo $file;
         }
